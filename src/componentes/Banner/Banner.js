da86845c43ede933b3674d1484fdc04b29b075f2
Liber Giacomi <114426434+libergiacomi@users.noreply.github.com>
2023-05-31 11:28:11 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    //Parece HTML mas é JSX
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
